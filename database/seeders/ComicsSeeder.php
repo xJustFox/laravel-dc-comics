c5c2987962c5b23343e571ad95bff405f94a8bc0
xJustFox <dodydaversa@gmail.com>
2024-02-16 14:40:35 +0100

    [Upgrade] Created file Model,Controller,Migration,Seeder and connected everything to the database passing the comics.php data

---
+++ database/seeders/ComicsSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Comic;
+
+class ComicsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $comics = config('comics');
+
+        foreach ($comics as $comic) {
+            $newComic = new Comic();
+            $newComic->title = $comic['title'];
+            $newComic->description = $comic['description'];
+            $newComic->thumb = $comic['thumb'];
+            $newComic->price = $comic['price'];
+            $newComic->series = $comic['series'];
+            $newComic->sale_date = $comic['sale_date'];
+            $newComic->type = $comic['type'];
+            $newComic->artists = json_encode($comic['artists']);
+            $newComic->writers = json_encode($comic['writers']);
+            $newComic->save();
+        }
+    }
+}
